d2589e0cfe60127e196c80fbf2701d9a40ffa455
karadahitotsu <karadahitotsu@gmail.com>
2024-05-17 15:28:42 +0300

    cart fix

--- src/main/resources/static/js/iflogin.js
+++ src/main/resources/static/js/iflogin.js
@@ -3,5 +3,5 @@ const userId = document.cookie.match(/userId=([^;]+)/)?.[1];
 if(userId){
     const cart = document.getElementById("carthref")
     cart.href =`/cart?userid=${userId}`
-
+    cart.classList.remove("invisible");
     }
